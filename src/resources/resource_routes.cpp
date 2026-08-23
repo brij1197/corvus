bc1f6322e195322267a1eb39610430812c981cf2
Brij Bhatia <brijbhatia997@gmail.com>
2026-08-23 17:00:56 +0530

    feat(CORV-38): POST /v1/resources (#159)

--- src/resources/resource_routes.cpp
+++ src/resources/resource_routes.cpp
@@ -1,4 +1,5 @@
 #include "corvus/resources/resource_routes.h"
+#include "corvus/resources/resource_handler.h"
 #include <drogon/drogon.h>
 
 namespace corvus::resources
@@ -7,6 +8,13 @@ namespace corvus::resources
     {
         (void)service;
 
-        LOG_INFO << "Resource routes registered (0 endpoints - pending CORV-37..41)";
+        auto create = create_resource_handler(service);
+        drogon::app().registerHandler(
+            "/v1/resources",
+            [create](const drogon::HttpRequestPtr &req,
+                     std::function<void(const drogon::HttpResponsePtr &)> &&cb)
+            { create(req, std::move(cb)); },
+            {drogon::Post});
+        LOG_INFO << "Resource routes registered (1 endpoint)";
     }
 } // namespace corvus::resources
